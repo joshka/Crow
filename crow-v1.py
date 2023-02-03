0aa0a612a3d399a17df3281de04ab805119bd50c
Josh McKinney <joshka@users.noreply.github.com>
2023-02-02 20:30:27 -0800

    Change to use the edit endpoint instead of completions

--- crow-v1.py
+++ crow-v1.py
@@ -1,52 +1,27 @@
-"""
-This is a small script called crow-v1.py that writes code that generates new versions of itself. It has a simple algorithm:
-
-1. Read an instruction from the user.
-2. Create a text prompt that contains the instruction and a copy of the current script.
-3. Call the openai completion api with the prompt, and the model parameter set to "code-davinci-002". The api key is stored in an environment variable named OPENAI_API_KEY.
-4. Save the output of the completion as a new script with filename version incremented.
-5. Output the name of the new script and instructions on how to run it. Do not run the script.
-"""
-
 import os
+import openai
+import re
 import sys
-import json
-import requests
 
-def get_script_name():
-    return os.path.basename(__file__)
+script_name = os.path.basename(__file__)
 
-def get_script_version():
-    return int(get_script_name().split('-')[-1].split('.')[0])
+script_code = open(script_name).read()
+instruction = sys.argv[1]
 
-def get_script_text():
-    with open(get_script_name(), 'r') as f:
-        return f.read()
+response = openai.Edit.create(
+        model="code-davinci-edit-001",
+        input=script_code,
+        instruction=instruction,
+        temperature=0)
+new_script_code = response["choices"][0]["text"]
 
-def get_prompt(instruction):
-    return instruction + '\n' + get_script_text()
+script_version = int(re.search(r"-v(\d+)\.py", script_name).group(1))
+script_name_no_version = script_name.split("-v")[0]
+new_script_name = f"{script_name_no_version}-v{script_version + 1}.py"
 
-def get_completion(prompt):
-    url = 'https://api.openai.com/v1/engines/davinci/completions'
-    headers = {'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY']}
-    data = {'prompt': prompt, 'max_tokens': 100, 'temperature': 0.7, 'top_p': 0.9, 'n': 1, 'stream': False, 'logprobs': None, 'stop': ['\n'], 'frequency_penalty': 0.0, 'presence_penalty': 0.0, 'temperature_bias': 0.0, 'model': 'code-davinci-002'}
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()['choices'][0]['text']
+with open(new_script_name, "w") as f:
+    f.write(new_script_code)
 
-def get_new_script_name():
-    return get_script_name().replace(str(get_script_version()), str(get_script_version() + 1))
-
-def write_new_script(script_text):
-    with open(get_new_script_name(), 'w') as f:
-        f.write(script_text)
-
-def main():
-    instruction = input('Enter an instruction: ')
-    prompt = get_prompt(instruction)
-    completion = get_completion(prompt)
-    write_new_script(completion)
-    print('New script: ' + get_new_script_name())
-    print('Run with: python ' + get_new_script_name())
-
-if __name__ == '__main__':
-    main()
+# Output information on how to run the new script.
+print(f"New script created: {new_script_name}")
+print(f"Run the new script with: python {new_script_name} <instruction>")
