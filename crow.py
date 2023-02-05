25f5b957d46b3632c3d297d4457a167adfe8b8c3
Josh McKinney <joshka@users.noreply.github.com>
2023-02-05 02:02:37 -0800

    Refactor the code

--- crow.py
+++ crow.py
@@ -6,11 +6,7 @@ from pygments import highlight
 from pygments.lexers import DiffLexer
 from pygments.formatters import TerminalFormatter
 
-def main():
-    instruction = input("Enter an instruction: ")
-
-    script_name = os.path.basename(__file__)
-    script_code = open(script_name).read()
+def edit(script_code, instruction):
     response = openai.Edit.create(
             model="code-davinci-edit-001",
             input=script_code,
@@ -19,16 +15,27 @@ def main():
 
     new_script_code = response["choices"][0]["text"]
 
-    diff = difflib.unified_diff(script_code.splitlines(keepends=True),
-                                new_script_code.splitlines(keepends=True),
-                                fromfile=script_name,
-                                tofile=script_name)
-    print(highlight("".join(diff), DiffLexer(), TerminalFormatter()))
+    return new_script_code
 
-    if input("Save changes? [y/N] ").lower() == "y":
-        with open(script_name, "w") as f:
-            f.write(new_script_code)
-        subprocess.run(["git", "commit", "-am", instruction])
+def main():
+    script_name = os.path.basename(__file__)
+    script_code = open(script_name).read()
+
+    while True:
+        instruction = input("Enter an instruction: ")
+        new_script_code = edit(script_code, instruction)
+
+        diff = difflib.unified_diff(script_code.splitlines(keepends=True),
+                                    new_script_code.splitlines(keepends=True),
+                                    fromfile=script_name,
+                                    tofile=script_name)
+        print(highlight("".join(diff), DiffLexer(), TerminalFormatter()))
+
+        if input("Save changes? [y/N] ").lower() == "y":
+            with open(script_name, "w") as f:
+                f.write(new_script_code)
+            subprocess.run(["git", "commit", "-am", instruction])
+            break
 
 if __name__ == "__main__":
     main()
