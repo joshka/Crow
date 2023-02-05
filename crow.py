45035c62a7caf09fc4f703d2a7e9a313ea191a14
Josh McKinney <joshka@users.noreply.github.com>
2023-02-05 01:58:12 -0800

    Add a main method

--- crow.py
+++ crow.py
@@ -6,25 +6,29 @@ from pygments import highlight
 from pygments.lexers import DiffLexer
 from pygments.formatters import TerminalFormatter
 
-instruction = input("Enter an instruction: ")
+def main():
+    instruction = input("Enter an instruction: ")
 
-script_name = os.path.basename(__file__)
-script_code = open(script_name).read()
-response = openai.Edit.create(
-        model="code-davinci-edit-001",
-        input=script_code,
-        instruction=instruction,
-        temperature=0)
+    script_name = os.path.basename(__file__)
+    script_code = open(script_name).read()
+    response = openai.Edit.create(
+            model="code-davinci-edit-001",
+            input=script_code,
+            instruction=instruction,
+            temperature=0)
 
-new_script_code = response["choices"][0]["text"]
+    new_script_code = response["choices"][0]["text"]
 
-diff = difflib.unified_diff(script_code.splitlines(keepends=True),
-                            new_script_code.splitlines(keepends=True),
-                            fromfile=script_name,
-                            tofile=script_name)
-print(highlight("".join(diff), DiffLexer(), TerminalFormatter()))
+    diff = difflib.unified_diff(script_code.splitlines(keepends=True),
+                                new_script_code.splitlines(keepends=True),
+                                fromfile=script_name,
+                                tofile=script_name)
+    print(highlight("".join(diff), DiffLexer(), TerminalFormatter()))
 
-if input("Save changes? [y/N] ").lower() == "y":
-    with open(script_name, "w") as f:
-        f.write(new_script_code)
-    subprocess.run(["git", "commit", "-am", instruction])
+    if input("Save changes? [y/N] ").lower() == "y":
+        with open(script_name, "w") as f:
+            f.write(new_script_code)
+        subprocess.run(["git", "commit", "-am", instruction])
+
+if __name__ == "__main__":
+    main()
